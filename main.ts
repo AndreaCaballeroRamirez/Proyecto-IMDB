acd07524faad050a3e692a1cf20997d40c3f60db
Andrea <andreacaballero.to@gmail.com>
2022-05-14 20:38:42 +0200

    Archivos ts con primera clase de miniproyecto

---
+++ main.ts
@@ -0,0 +1,11 @@
+import { Professional } from "./Professional";
+
+let persona1: Professional= new Professional ("Carla", 23, "Female", 65, 168, "azul", "marron","caucasica",false,"Española",77378262, "Escritora")
+let persona2: Professional= new Professional ("David", 30, "Male", 80, 176,"Negro","azules","asiatico",false,"japones",95756864,"locurtor")
+let persona3: Professional= new Professional ("Ricardo", 89, "Male", 78, 180,"gris","negros","caucasico",true,"español",3746564,"Agricultor")
+let persona4: Professional= new Professional ("Laura",46,"male",65,170,"castaña","azules","asiatica",false,"china",8746438,"pintora")
+
+persona1.print ()
+persona2.print()
+persona3.print()
+persona4.print()
